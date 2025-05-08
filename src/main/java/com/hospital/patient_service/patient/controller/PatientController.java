845a73474e2e06b7d2b84baf78ef03d102a69628
Amar-Jeet-sahi <amarjeet.sahi70@gmail.com>
2025-05-09 03:56:24 +0530

    Delete API

--- src/main/java/com/hospital/patient_service/patient/controller/PatientController.java
+++ src/main/java/com/hospital/patient_service/patient/controller/PatientController.java
@@ -2,12 +2,16 @@ package com.hospital.patient_service.patient.controller;
 
 import com.hospital.patient_service.patient.dto.PatientDTO;
 import com.hospital.patient_service.patient.dto.PatientRequestDto;
+import com.hospital.patient_service.patient.model.Patient;
+import com.hospital.patient_service.patient.repository.PatientRepository;
 import com.hospital.patient_service.patient.service.PatientService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/patient")
@@ -29,5 +33,13 @@ public class PatientController {
     ){
 
         return ResponseEntity.ok().body(patientService.insertPatient(patientRequestDto));
+    }
+    @DeleteMapping( "{id}")
+    public ResponseEntity<String> deletePatient(@PathVariable UUID id ) throws SQLException {
+
+        if(patientService.removePatientByID(id)){
+            return ResponseEntity.ok("Patient removed successfully");
+        }
+        return ResponseEntity.internalServerError().body("Something went wrong");
     }
 }
